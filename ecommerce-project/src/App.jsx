f3435c38d720a3aa0c98b82fac838ee15f439b21
BIKO <bikilabirhanu059@gmail.com>
2025-11-20 22:17:01 +0300

    Move Track page to React

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -3,6 +3,7 @@ import {Routes, Route} from 'react-router';
 import {HomePage} from "./Pages/HomePage.jsx"
 import { CheckoutPage } from './Pages/CheckoutPage.jsx';
 import { OrdersPage } from './Pages/OrdersPage.jsx';
+import {TrackingPage} from "./Pages/TrackingPage.jsx";
 
 function App() {
   return(
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<HomePage />}/>
       <Route path="checkout" element={<CheckoutPage/>}/>
       <Route path="orders" element={<OrdersPage />}/>
+      <Route path="track" element={<TrackingPage />}/>
     </Routes>
   );
 }
